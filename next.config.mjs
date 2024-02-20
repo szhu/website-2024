952bdb33db055656646c3741771c0f092e4927aa
Sean Zhu <interestinglythere@gmail.com>
2024-02-20 02:41:44 -0500

    Fix eslint errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+// eslint-disable-next-line consistent-default-export-name/default-export-match-filename
 const nextConfig = {};
 
 export default nextConfig;
