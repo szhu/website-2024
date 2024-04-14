6a91596ce4762fe25486a75f458e4a98a1806554
Sean Zhu <interestinglythere@gmail.com>
2024-04-14 18:03:18 -0400

    WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-"use client";
-import styled from "styled-components";
-
-const RedText = styled.span`
-  color: red;
-`;
-
 export default function page() {
-  return <RedText className="text-3xl">Test!</RedText>;
+  return <div className="text-3xl text-red-500">Test!</div>;
 }
