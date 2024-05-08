b51748c550f83104fcbbe03c99d084a68d2811e8
Sean Zhu <interestinglythere@gmail.com>
2024-05-08 05:27:51 -0400

    WIP

--- src/home/ItemView.tsx
+++ src/home/ItemView.tsx
@@ -1,37 +1,129 @@
 "use client";
 import { useParams } from "next/navigation";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { twMerge } from "tailwind-merge";
 import getDataUrlFromFile from "../extends/file/getDataUrlFromFile";
 import { useDidPropChangeAcrossRoutes } from "../extends/next/TrackPropsAcrossRoutes";
 
 function insertNodeAt(range: Range, node: Node) {
-  range.insertNode(document.createElement("br"));
-  range.insertNode(document.createElement("br"));
   range.insertNode(node);
-  range.insertNode(document.createElement("br"));
-  range.insertNode(document.createElement("br"));
   range.setStartAfter(range.startContainer);
+}
+
+function compareElementPositionToCaret(element: Node): number | undefined {
+  const selection = window.getSelection();
+  if (!selection) return undefined;
+
+  const range = selection.getRangeAt(0);
+
+  if (
+    range.startContainer.compareDocumentPosition(element) ===
+    Node.DOCUMENT_POSITION_PRECEDING
+  ) {
+    return -1;
+  } else if (
+    range.endContainer.compareDocumentPosition(element) ===
+    Node.DOCUMENT_POSITION_FOLLOWING
+  ) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+/**
+ * 1. Outdent the element, splitting its parent into a "before" and "after" element
+ * 2. If either the "before" or "after" element is empty, remove it
+ */
+function unnestChild(targetChild: Node, transform?: (child: Node) => Node) {
+  const container = targetChild.parentElement;
+  if (!container) return;
+
+  const parent = container.parentElement;
+  if (!parent) return;
+
+  if ((compareElementPositionToCaret(targetChild) ?? 0) < 0) {
+    const before = document.createElement(container.tagName);
+    const after = container;
+    while (after.firstChild) {
+      const child = after.firstChild;
+      if (child === targetChild) {
+        child.remove();
+        break;
+      }
+
+      before.append(child);
+    }
+
+    if (before.hasChildNodes()) {
+      after.before(before);
+    }
+
+    after.before(transform ? transform(targetChild) : targetChild);
+
+    if (!after.hasChildNodes()) {
+      after.remove();
+    }
+  } else {
+    const before = container;
+    const after = document.createElement(before.tagName);
+
+    let isAfter = false;
+    for (const child of before.childNodes) {
+      if (child === targetChild) {
+        child.remove();
+        isAfter = true;
+      }
+
+      if (isAfter) {
+        after.append(child);
+      }
+    }
+
+    if (after.hasChildNodes()) {
+      before.after(after);
+    }
+
+    before.after(transform ? transform(targetChild) : targetChild);
+
+    if (!before.hasChildNodes()) {
+      before.remove();
+    }
+  }
+}
+
+function moveChildren(from: Element, to: Element) {
+  while (from.firstChild) {
+    to.append(from.firstChild);
+  }
+}
+
+function unwrapElement(element: Element) {
+  const parent = element.parentElement;
+  if (!parent) return;
+
+  while (element.firstChild) {
+    parent.insertBefore(element.firstChild, element);
+  }
+  element.remove();
 }
 
 const ContentEditable: React.FC<{
   className?: string;
   onUpdate?: (target: HTMLElement) => void;
 }> = (props) => {
-  const ref = useRef<HTMLDivElement>(null);
+  const ref = useRef<HTMLDivElement | null>(null);
 
   const [isDraggedOver, setIsDraggedOver] = useState(false);
 
-  const initialOnUpdateRef = useRef(props.onUpdate);
-  useEffect(() => {
-    if (ref.current == null) return;
-
-    initialOnUpdateRef.current?.(ref.current);
-  }, []);
-
   return (
     <div
-      ref={ref}
+      ref={(element) => {
+        ref.current = element;
+        if (element) {
+          props.onUpdate?.(element);
+        }
+      }}
       contentEditable
       className={twMerge(
         "relative",
@@ -124,8 +216,72 @@ const ItemView: React.FC<{
       <div>{params.itemId}</div>
 
       <ContentEditable
-        className="min-h-20 rounded-md border-1 border-black p-1 dark:border-white"
+        className="min-h-20 border-collapse rounded-md border-1 border-black p-1 dark:border-white [&>p+p]:border-t-0 [&>p]:border-y-1 [&>p]:border-gray-500 [&>p]:py-2"
         onUpdate={(target) => {
+          {
+            // Remove all style and class attributes
+            const els = target.querySelectorAll("[style],[class]");
+            for (const element of els) {
+              element.removeAttribute("style");
+              element.removeAttribute("class");
+            }
+          }
+
+          {
+            // Unwrap all <div> and <span>
+            const els = target.querySelectorAll("div,span");
+            for (const element of els) {
+              unwrapElement(element);
+            }
+          }
+
+          {
+            // Unnest all <img>
+            const els = target.querySelectorAll(":scope > * > img");
+            for (const element of els) {
+              if (
+                !element.parentElement ||
+                element.parentElement.childNodes.length <= 1
+              ) {
+                return;
+              }
+
+              unnestChild(element, (img) => {
+                const p = document.createElement("p");
+                p.append(img);
+                return p;
+              });
+            }
+          }
+
+          {
+            // Wrap all outer non-<p> in <p>
+            const children = [...target.childNodes];
+            for (const child of children) {
+              if (child instanceof HTMLParagraphElement) continue;
+
+              const p = document.createElement("p");
+              p.append(child);
+              target.append(p);
+            }
+          }
+
+          {
+            // If there is no content, add an empty <p>
+            if (target.childElementCount === 0) {
+              const p = document.createElement("p");
+              target.append(p);
+            }
+          }
+
+          {
+            // Put a <br> inside every empty <p>
+            const els = target.querySelectorAll("p:empty");
+            for (const element of els) {
+              element.append(document.createElement("br"));
+            }
+          }
+
           htmlRef.current = target.innerHTML;
         }}
       />
