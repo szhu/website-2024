2017957e03449cacf061d0bba9303f7fb0e72fd7
Sean Zhu <interestinglythere@gmail.com>
2024-05-09 03:38:50 -0400

    WIP

---
+++ src/debug/ContentEditable.tsx
@@ -0,0 +1,107 @@
+import { useEffect, useRef, useState } from "react";
+import { twMerge } from "tailwind-merge";
+import getDataUrlFromFile from "../extends/file/getDataUrlFromFile";
+import { insertNodesAt } from "../items/DomManipulation";
+
+function useIsHydrated() {
+  const [isHydrated, setIsHydrated] = useState(false);
+
+  useEffect(() => {
+    setIsHydrated(true);
+  }, []);
+
+  return isHydrated;
+}
+
+const ContentEditable: React.FC<{
+  editorRef: React.MutableRefObject<HTMLElement | null>;
+  className?: string;
+  onClick?: (event: React.MouseEvent<HTMLDivElement>) => void;
+  onUpdate?: (target: HTMLElement) => void;
+}> = (props) => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const isHydrated = useIsHydrated();
+
+  const [isDraggedOver, setIsDraggedOver] = useState(false);
+
+  return (
+    <div
+      ref={(element) => {
+        ref.current = element;
+        props.editorRef.current = element;
+        if (element) {
+          props.onUpdate?.(element);
+        }
+      }}
+      contentEditable={isHydrated}
+      className={twMerge(
+        "relative",
+        isDraggedOver &&
+          // eslint-disable-next-line prefer-smart-quotes/prefer
+          "before:absolute before:inset-0 before:flex before:items-center before:justify-center before:bg-yellow-500/50 before:content-['Insert_Image']",
+        "selection:bg-blue-200 selection:text-black",
+        props.className,
+      )}
+      onClick={props.onClick}
+      onDragOver={() => {
+        const selection = window.getSelection();
+        if (!selection) return;
+
+        if (selection.rangeCount < 1) return;
+
+        setIsDraggedOver(true);
+      }}
+      onDragLeave={() => {
+        setIsDraggedOver(false);
+      }}
+      onDragEnd={() => {
+        setIsDraggedOver(false);
+      }}
+      onDrop={async (event) => {
+        setIsDraggedOver(false);
+        const element = event.currentTarget;
+
+        event.preventDefault();
+
+        const imgsToInsert: HTMLImageElement[] = [];
+        const files = event.dataTransfer.files;
+        for (const file of files) {
+          if (!file.type.startsWith("image/")) continue;
+
+          const dataUrl = await getDataUrlFromFile(file);
+
+          const img = document.createElement("img");
+          img.src = dataUrl;
+
+          imgsToInsert.push(img);
+        }
+
+        element.focus();
+        const selection = window.getSelection();
+        if (!selection) {
+          console.warn("No selection");
+          return;
+        }
+
+        selection.collapseToEnd();
+        const range = selection.getRangeAt(0);
+        if (
+          !element.contains(range.startContainer) ||
+          !element.contains(range.endContainer)
+        ) {
+          console.warn("Selection is outside of container");
+          return;
+        }
+
+        insertNodesAt(range, imgsToInsert);
+
+        props.onUpdate?.(element);
+      }}
+      onInput={(event) => {
+        props.onUpdate?.(event.currentTarget);
+      }}
+    />
+  );
+};
+
+export default ContentEditable;
---
+++ src/items/ItemEditor.tsx
@@ -0,0 +1,155 @@
+import { useRef, useState } from "react";
+import ContentEditable from "../debug/ContentEditable";
+import getDataUrlFromFile from "../extends/file/getDataUrlFromFile";
+import { unnestChild, unwrapElement } from "./DomManipulation";
+
+export default function ItemEditor() {
+  const editorRef = useRef<HTMLElement>(null);
+  const [copiedAt, setCopiedAt] = useState<Date>();
+
+  return (
+    <>
+      <ContentEditable
+        editorRef={editorRef}
+        className="min-h-40 border-collapse whitespace-pre-wrap rounded-md border-1 border-black p-1 dark:border-white [&>p+p]:border-t-0 [&>p:first-child]:border-t-0 [&>p:has(>img)]:px-6  [&>p:last-child]:border-b-0 [&>p>img]:mx-auto [&>p>img]:max-h-72 [&>p>img]:cursor-pointer [&>p>img]:px-1 [&>p]:border-y-1 [&>p]:border-gray-500 [&>p]:py-2"
+        onClick={(event) => {
+          if (event.target instanceof HTMLImageElement) {
+            // Select the image if clicked
+            const selection = window.getSelection();
+            if (!selection) return;
+
+            const range = document.createRange();
+            range.selectNode(event.target);
+
+            selection.removeAllRanges();
+            selection.addRange(range);
+          }
+        }}
+        onUpdate={(target) => {
+          {
+            // Remove all style and class attributes
+            const els = target.querySelectorAll("[style],[class]");
+            for (const element of els) {
+              element.removeAttribute("style");
+              element.removeAttribute("class");
+            }
+          }
+
+          {
+            // Unwrap all <div> and <span>
+            const els = target.querySelectorAll(":not(p,img)");
+            for (const element of els) {
+              unwrapElement(element);
+            }
+          }
+
+          {
+            // Unnest all <img>
+            const els = target.querySelectorAll(":scope > * > img");
+            for (const element of els) {
+              if (
+                !element.parentElement ||
+                element.parentElement.childNodes.length <= 1
+              ) {
+                continue;
+              }
+
+              unnestChild(element, (img) => {
+                const p = document.createElement("p");
+                p.append(img);
+                return p;
+              });
+            }
+          }
+
+          {
+            // Inline all img srcs
+            const els = target.querySelectorAll(
+              `img[src^='http:'],img[src^='https:']`,
+            );
+            void Promise.resolve().then(async () => {
+              for (const element of els) {
+                if (!(element instanceof HTMLImageElement)) continue;
+
+                const src = element.getAttribute("src");
+                if (!src) continue;
+
+                // const dataUrl = await window.fetch(src)
+                //   .then((res) => res.blob())
+                //   .then(getDataUrlFromFile);
+                const response = await window.fetch(src);
+                const blob = await response.blob();
+                const dataUrl = await getDataUrlFromFile(blob);
+
+                element.src = dataUrl;
+              }
+            });
+          }
+
+          {
+            // Wrap all outer non-<p> in <p>
+            const children = [...target.childNodes];
+            for (const child of children) {
+              if (child instanceof HTMLParagraphElement) continue;
+
+              const p = document.createElement("p");
+              child.replaceWith(p);
+              p.append(child);
+            }
+          }
+
+          {
+            // For every empty <p>, add a <br> if the cursor is inside it;
+            // otherwise, remove it.
+            const els = target.querySelectorAll("p");
+            for (const element of els) {
+              const isEmpty = element.childNodes.length === 0;
+              const hasOnlySingleBr =
+                element.childNodes.length === 1 &&
+                element.firstChild?.nodeName === "BR";
+
+              if (isEmpty || hasOnlySingleBr) {
+                const selection = window.getSelection();
+                if (!selection) return;
+
+                if (element.contains(selection.anchorNode)) {
+                  if (isEmpty) {
+                    element.append(document.createElement("br"));
+                  }
+                } else {
+                  element.remove();
+                }
+              }
+            }
+          }
+
+          {
+            // If there is no content, add an empty <p>
+            if (target.childElementCount === 0) {
+              const p = document.createElement("p");
+              p.append(document.createElement("br"));
+              target.append(p);
+            }
+          }
+        }}
+      />
+
+      <button
+        onClick={async () => {
+          await navigator.clipboard.writeText(
+            editorRef.current?.innerHTML ?? "",
+          );
+          setCopiedAt(new Date());
+        }}
+      >
+        Copy HTML{" "}
+        <span
+          key={copiedAt?.toISOString()}
+          className={copiedAt ? "animate-fade" : "invisible"}
+        >
+          ✔
+        </span>
+      </button>
+    </>
+  );
+}
