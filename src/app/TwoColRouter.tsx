8573311448fcae3261038b737a3d091579fc2253
Sean Zhu <interestinglythere@gmail.com>
2024-05-14 03:34:14 -0400

    Site fixes

--- src/app/TwoColRouter.tsx
+++ src/app/TwoColRouter.tsx
@@ -1,11 +1,36 @@
 "use client";
+import { usePathname } from "next/navigation";
+import { useEffect, useRef } from "react";
+import SiteTitle from "../listings/SiteTitle";
 import { useNavContext } from "../navigation/NavContext";
 import TwoColPage from "./TwoColPage";
 
 const TwoColRouter: React.FC<{
   children?: React.ReactNode;
 }> = (props) => {
+  const pathname = usePathname();
+  const previousPathnameRef = useRef<string>();
   const nav = useNavContext();
+
+  // Hack! Sometimes it seems like Next.js is unintentionally removing the
+  // <title> tag when navigating between two pages that use the same layout.
+  // Here we add it back.
+  useEffect(() => {
+    document.title = SiteTitle;
+  }, [nav]);
+
+  // Hack! On mobile, clicking links doesn't scroll to the top of the page. We
+  // fix this by scrolling to the top of the page whenever the pathname changes.
+  useEffect(() => {
+    if (
+      previousPathnameRef.current != null &&
+      previousPathnameRef.current !== pathname
+    ) {
+      document.documentElement.scrollTop = 0;
+    }
+
+    previousPathnameRef.current = pathname;
+  }, [nav, pathname]);
 
   const notFound = props.children;
 
