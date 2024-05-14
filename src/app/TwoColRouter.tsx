e1b95c8b10a843d3be4945722a041b92d159cbfe
Sean Zhu <interestinglythere@gmail.com>
2024-05-14 04:10:11 -0400

    WIP

--- src/app/TwoColRouter.tsx
+++ src/app/TwoColRouter.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { useEffect } from "react";
+import { usePathname } from "next/navigation";
+import { useEffect, useRef } from "react";
 import SiteTitle from "../listings/SiteTitle";
 import { useNavContext } from "../navigation/NavContext";
 import TwoColPage from "./TwoColPage";
@@ -7,6 +8,8 @@ import TwoColPage from "./TwoColPage";
 const TwoColRouter: React.FC<{
   children?: React.ReactNode;
 }> = (props) => {
+  const pathname = usePathname();
+  const previousPathnameRef = useRef<string>();
   const nav = useNavContext();
 
   // Hack! Sometimes it seems like Next.js is unintentionally removing the
@@ -15,6 +18,19 @@ const TwoColRouter: React.FC<{
   useEffect(() => {
     document.title = SiteTitle;
   }, [nav]);
+
+  // Hack! On mobile, clicking links doesn't scroll to the top of the page. We
+  // fix this by scrolling to the top of the page whenever the pathname changes.
+  useEffect(() => {
+    if (
+      previousPathnameRef.current != null &&
+      previousPathnameRef.current !== pathname
+    ) {
+      document.documentElement.scrollTop = 0;
+    }
+
+    previousPathnameRef.current = pathname;
+  }, [nav, pathname]);
 
   const notFound = props.children;
 
