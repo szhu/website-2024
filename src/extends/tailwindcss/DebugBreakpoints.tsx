fa0c14b528931488261ed33da4a0e0eedaf3e349
Sean Zhu <interestinglythere@gmail.com>
2024-05-16 23:03:30 -0400

    Add base size to breakpoint debug tool

--- src/extends/tailwindcss/DebugBreakpoints.tsx
+++ src/extends/tailwindcss/DebugBreakpoints.tsx
@@ -14,7 +14,7 @@ const DebugBreakpoints: React.FC<unknown> = () => {
   return (
     isVisible && (
       <div
-        className={`pointer-events-none fixed left-0 top-0 z-50 p-1 font-bold text-red-500 opacity-80 before:content-["2xs"] xs:before:content-["xs"] sm:before:content-["sm"] md:before:content-["md"] lg:before:content-["lg"] xl:before:content-["xl"]`}
+        className={`pointer-events-none fixed left-0 top-0 z-50 p-1 font-bold text-red-500 opacity-80 before:content-["<2xs"] 2xs:before:content-["2xs"] xs:before:content-["xs"] sm:before:content-["sm"] md:before:content-["md"] lg:before:content-["lg"] xl:before:content-["xl"]`}
       />
     )
   );
